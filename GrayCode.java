452c93675b888fdc14cf4dc71bbc3755d42789b2
sujidasimmanuel <79018233+sujidasimmanuel@users.noreply.github.com>
2021-02-13 23:09:06 +0530

    Create GrayCode.java

---
+++ GrayCode.java
@@ -0,0 +1,50 @@
+ 
+import java.util.*;
+class GrayCode { 
+
+ 
+static void generateGrayarr(int n) 
+{ 
+	
+	if (n <= 0) 
+		return; 
+
+	
+	ArrayList<String> arr = new ArrayList<String> (); 
+
+	
+	arr.add("0"); 
+	arr.add("1"); 
+
+	
+	int i, j; 
+	for (i = 2; i < (1<<n); i = i<<1) 
+	{ 
+		
+		for (j = i-1 ; j >= 0 ; j--) 
+			arr.add(arr.get(j)); 
+
+		
+		for (j = 0 ; j < i ; j++) 
+			arr.set(j, "0" + arr.get(j)); 
+
+		
+		for (j = i ; j < 2*i ; j++) 
+			arr.set(j, "1" + arr.get(j)); 
+	} 
+
+	
+	for (i = 0 ; i < arr.size() ; i++ ) 
+		System.out.println(arr.get(i)); 
+} 
+
+
+public static void main(String[] args) 
+{ 
+	int k;
+    System.out.println("Enter a value less than 15");
+     Scanner sc = new Scanner(System.in); 
+    k=sc.nextInt();
+    generateGrayarr(k); 
+}
+} 
